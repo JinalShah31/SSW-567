8d1a35a29c642d5f87faeda0497b618b3f5b28d6
JinalShah31 <jinalsanghrajka@gmail.com>
2024-10-04 18:38:32 -0400

    Fixed buggy triangle program

--- Triangle567/Triangle.py
+++ Triangle567/Triangle.py
@@ -12,9 +12,33 @@ The primary goal of this file is to demonstrate a simple python program to class
 def classifyTriangle(a,b,c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
+    you test cases. """
+
+    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+        return 'InvalidInput'
     
-    This function returns a string with the type of triangle from three integer values
+    if a > 200 or b > 200 or c > 200:
+        return 'InvalidInput'
+        
+    if a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
+      
+    # the sum of any two sides must be strictly less than the third side
+    # of the specified shape is not a triangle
+    if (a + b <= c) or (a + c <= b) or (b + c <= a):
+        return 'NotATriangle'
+        
+    # now we know that we have a valid triangle 
+    if a == b == c:
+        return 'Equilateral'
+    elif (a**2 + b**2 == c**2) or (a**2 + c**2 == b**2) or (b**2 + c**2 == a**2):
+        return 'Right'
+    elif a == b or b == c or a == c:
+        return 'Isosceles'
+    else:
+        return 'Scalene'
+    
+    """ This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
     
     return:
@@ -25,7 +49,7 @@ def classifyTriangle(a,b,c):
         If the sum of any two sides equals the squate of the third side, then return 'Right'
       
       BEWARE: there may be a bug or two in this code
-    """
+   
 
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
@@ -55,3 +79,5 @@ def classifyTriangle(a,b,c):
         return 'Scalene'
     else:
         return 'Isoceles'
+
+"""
